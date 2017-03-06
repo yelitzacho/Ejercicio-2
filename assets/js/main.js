8027b328e770bd54c72a0b88cc9e75f09c24dafc
Yelitza Cho <yelitza.choque@gmail.com>
2017-03-05 19:23:37 -0500

    Revisando funciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
-var datosLaboratoria = [{ nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero"},
-             { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre"},
-             { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo"},
-             { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
-             { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" }];
+var objeto = [{ nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero"},
+              { nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre"},
+              { nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo"},
+              { nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+              { nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" }];
 
-var resultado = datosLaboratoria.forEach(function(indice,datos){
-  documernt.getElementById("registro").innerHTML+=  ("<ul>"
-                                                    "<li>Nombre:" + datos[indice].nombre + "</li>"
-                                                    "<li>Apellidos:" datos[indice].apellido + "<li>"
-                                                    "<li>Rol:" datos[indice].rol + "<li>"
-                                                    "</li><li> Cumpleaños:"datos[indice].cumpleanios +"</li>"
-                                                    "</ul>");
-};
+var resultado = "";
+function mostrar_nombre(){
+  for(var i in objeto){
+    resultado +=  "<li>Nombre:" + objeto[i].nombre + "<br>" +
+                  "<li>Apellidos:" objeto[i].apellido + "<br>" +
+                  "<li>Rol:" objeto[i].rol + "<br>"+
+                  "<li>Cumpleaños:" objeto[i].cumpleanios + "<br>" + "<br>";
+  }
+  document.getElementById("imprimir").innerHTML = resultado;
+}
